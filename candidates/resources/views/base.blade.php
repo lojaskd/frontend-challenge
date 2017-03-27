1eea180f061587d05ee2194610551ef1db64485f
rafapaulin <rafapaulin@gmail.com>
2017-03-27 16:59:23 +0000

    Initial setup, dependencies, templates and libraries

---
+++ candidates/resources/views/base.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html html lang="pt-BR">
+	<head>
+		<title>LojasKD: Sua Loja de Móveis Online! - @yield('title')</title>
+		@include('base.meta')
+
+		<link href="https://fonts.googleapis.com/css?family=Lato:400,700" rel="stylesheet"> 
+
+		@include('base.styles')
+	</head>
+	<body>
+		@include('base.scripts')
+	</body>
+</html>
